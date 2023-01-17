cf37416da8d05bf7fda911706213cccece28ba42
Antoine MARIONNEAU <antoine.marionneau@aprilmarine.com>
2023-01-17 16:32:48 +0100

    Add constraint on budget date

--- src/Validator/Constraints/UniqueMonthBudgetValidator.php
+++ src/Validator/Constraints/UniqueMonthBudgetValidator.php
@@ -2,14 +2,42 @@
 
 namespace App\Validator\Constraints;
 
+use App\Repository\BudgetRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use UnexpectedValueException;
+
 final class UniqueMonthBudgetValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint): void
+    public function __construct(private readonly BudgetRepository $budgetRepository)
     {
-        // TODO: Check if Month is Unique
-        if (!array_diff(['description', 'price'], $value)) {
+    }
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof UniqueMonthBudget) {
+            throw new UnexpectedTypeException($constraint, UniqueMonthBudget::class);
+        }
+
+        if ($value === null) {
+            throw new UnexpectedValueException('The date value should not be null for budgets', null);
+        }
+
+        if (!$value instanceof \DateTime) {
+            throw new UnexpectedValueException($value, \DateTime::class);
+        }
+
+        $budgets = $this->budgetRepository->findAll();
+
+        foreach ($budgets as $budget) {
+            if ($budget->getFormatedDate() === $value->format('m/y')) {
+                $throwError = true;
+            }
+        }
+
+        if ($throwError ?? false) {
             $this->context->buildViolation($constraint->message)->addViolation();
         }
     }
