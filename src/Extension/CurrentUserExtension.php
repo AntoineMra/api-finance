17541321be6c52b1acac5f7259f0a08dd7373905
Antoine Marionneau <antoine.marionneau.1629@gmail.com>
2023-08-24 13:09:30 +0200

    feat(current-user): add same structure for item

--- src/Extension/CurrentUserExtension.php
+++ src/Extension/CurrentUserExtension.php
@@ -7,14 +7,22 @@ use ApiPlatform\Metadata\Operation;
 use Symfony\Bundle\SecurityBundle\Security;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 
-class CurrentUserExtension implements QueryCollectionExtensionInterface
+class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
     public function __construct(private readonly Security $security)
     {
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
+    {
+        $user = $this->security->getUser();
+        // Needs implementation of createdBy on all entities
+        // HERE I must check if createdBy and current user match => to be used by all entities
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass,array $identifiers, Operation $operation = null, array $context = []): void
     {
         $user = $this->security->getUser();
         // Needs implementation of createdBy on all entities
